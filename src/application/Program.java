4f412a908e921a4576f6b1461d3d86b94c14bcce
Welberrr <welberhenrique.costa@gmail.com>
2024-01-12 20:22:52 -0300

    Finalizando projeto

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 import entities.Contract;
 import entities.Installment;
 import services.ContractService;
+import services.PayPalService;
 
 public class Program {
 
@@ -31,7 +32,7 @@ public class Program {
 		System.out.print("Entre com um número de parcelas: ");
 		int n = sc.nextInt();
 		
-		ContractService contractService = new ContractService(null);
+		ContractService contractService = new ContractService(new PayPalService());
 		
 		contractService.processContract(obj, n);
 		
